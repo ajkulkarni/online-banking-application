afa9a12526da11f034491916088def12fd0e1641
amrit <amritsahoo@gmail.com>
2016-10-16 14:50:46 -0700

    fixing typos with merge

--- src/org/thothlab/devilsvault/logincontrollers/LoginController.java
+++ src/org/thothlab/devilsvault/logincontrollers/LoginController.java
@@ -1,18 +1,15 @@
 package org.thothlab.devilsvault.logincontrollers;
 
-<<<<<<< HEAD
 import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-=======
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
->>>>>>> master
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
