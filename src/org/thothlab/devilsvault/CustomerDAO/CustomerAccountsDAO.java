715292edb3508bbc86379faa2d7085967ae836c4
RakeshKatkam <rrkatkam@asu.edu>
2016-10-16 14:58:25 -0700

    account display done

---
+++ src/org/thothlab/devilsvault/CustomerDAO/CustomerAccountsDAO.java
@@ -0,0 +1,62 @@
+package org.thothlab.devilsvault.CustomerDAO;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+import org.thothlab.devilsvault.CustomerModel.Customer;
+import org.thothlab.devilsvault.CustomerModel.TransactionModel;
+import org.thothlab.devilsvault.CustomerModel.TransactionModel2;
+@Repository ("CustomerAccountsDAO")
+public class CustomerAccountsDAO{
+	private JdbcTemplate jdbcTemplate;
+	private DataSource dataSource;
+	@Autowired
+	public void setdataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+	public List<TransactionModel2> getTransactionLines(Customer user,int interval)
+	{
+		Connection con = null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		TransactionModel2 TransactionLine = new TransactionModel2();
+		List<TransactionModel2> TransactionLines = new ArrayList<TransactionModel2>(); 
+		String sql ="SELECT * FROM  transaction_completed WHERE payer_id="+user.getID()
+				+"OR payee_id = "+user.getID()
+				+"AND "+TransactionLine.getTimestamp_updated()+" between DATE_SUB(NOW(), INTERVAL "+interval+" MONTH) AND NOW()";
+		try{
+			con = dataSource.getConnection();
+			ps = con.prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next())
+			{
+				TransactionLine = new TransactionModel2();
+				
+				TransactionLine.setDescription(rs.getString("description"));
+				int id = rs.getInt(0);
+				//TransactionLine.setID(id);
+				TransactionLines.add(TransactionLine);
+			}
+		}catch(SQLException e){
+			e.printStackTrace();
+		}finally{
+			try {
+				rs.close();
+				ps.close();
+				con.close();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return TransactionLines;
+     }     
+}
+
---
+++ src/org/thothlab/devilsvault/customercontrollers/CustomerAccountsController.java
@@ -0,0 +1,72 @@
+package org.thothlab.devilsvault.customercontrollers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+import org.thothlab.devilsvault.CustomerDAO.*;
+import org.thothlab.devilsvault.CustomerModel.CreditAccount;
+import org.thothlab.devilsvault.CustomerModel.Customer;
+import org.thothlab.devilsvault.CustomerModel.DebitAccount;
+import org.thothlab.devilsvault.CustomerModel.TransactionModel;
+import org.thothlab.devilsvault.CustomerModel.BankAccount.AccountType;
+
+@Controller
+public class CustomerAccountsController {
+	
+	@RequestMapping("/customerSavingsAccount")
+	public ModelAndView helloworld(){
+		DebitAccount checkingAccount = new DebitAccount(AccountType.CHECKING);
+		checkingAccount.setAccountNumber(123);
+		DebitAccount savingAccount = new DebitAccount(AccountType.SAVINGS);
+		savingAccount.setAccountNumber(100);
+		CreditAccount creditAccount = new CreditAccount();
+		creditAccount.setAccountNumber(102);
+		Customer customer = new Customer();
+		customer.setID(101);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("DaoDetails.xml");
+		ExtUserDaoImpl CustomerDAO = ctx.getBean("ExtUserDaoImpl", ExtUserDaoImpl.class);
+		Double SavingsAccBal = CustomerDAO.getSavingsBalance(customer);
+		CustomerAccountsDAO sAccountDAO = ctx.getBean("CustomerAccountsDAO",CustomerAccountsDAO.class);
+		List<TransactionModel> TransactionLines = new ArrayList<TransactionModel>();
+		//TransactionLines = sAccountDAO.getTransactionLines(customer, 1);
+		ctx.close();
+		ModelAndView model = new ModelAndView("customerPages/accountsSavingsPage");
+		model.addObject("Customer",customer);
+		model.addObject("cAccount", checkingAccount );
+		model.addObject("sAccount", savingAccount );
+		model.addObject("ccAccount", creditAccount );
+		model.addObject("SavingsAccBal",SavingsAccBal);
+		model.addObject("TransactionLines",TransactionLines);
+		return model;
+	}
+	@RequestMapping("/customerCheckingAccount")
+	public ModelAndView checkingAccount(){
+		DebitAccount checkingAccount = new DebitAccount(AccountType.CHECKING);
+		checkingAccount.setAccountNumber(123);
+		DebitAccount savingAccount = new DebitAccount(AccountType.SAVINGS);
+		savingAccount.setAccountNumber(100);
+		CreditAccount creditAccount = new CreditAccount();
+		creditAccount.setAccountNumber(102);
+		Customer customer = new Customer();
+		customer.setID(101);
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("DaoDetails.xml");
+		ExtUserDaoImpl CustomerDAO = ctx.getBean("ExtUserDaoImpl", ExtUserDaoImpl.class);
+		Double CheckingAcctBal = CustomerDAO.getCheckingBalance(customer);
+		CustomerAccountsDAO sAccountDAO = ctx.getBean("CustomerAccountsDAO",CustomerAccountsDAO.class);
+		List<TransactionModel> TransactionLines = new ArrayList<TransactionModel>();
+		//TransactionLines = sAccountDAO.getTransactionLines(customer, 1);
+		ctx.close();
+		ModelAndView model = new ModelAndView("customerPages/accountsCheckingPage");
+		model.addObject("Customer",customer);
+		model.addObject("cAccount", checkingAccount );
+		model.addObject("sAccount", savingAccount );
+		model.addObject("ccAccount", creditAccount );
+		model.addObject("CheckingAccBal", CheckingAcctBal);
+		model.addObject("TransactionLines",TransactionLines);
+		return model;
+	}
+}
