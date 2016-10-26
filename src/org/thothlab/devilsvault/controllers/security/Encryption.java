83f13f2f7c1742f48e2f1e96cab96d2d6c9fab2a
nakapika <aakulka9@asu.edu>
2016-10-26 12:43:17 -0700

    Encryption AES Module

--- src/org/thothlab/devilsvault/controllers/security/Encryption.java
+++ src/org/thothlab/devilsvault/controllers/security/Encryption.java
@@ -17,70 +17,63 @@ import javax.crypto.spec.SecretKeySpec;
 import org.apache.commons.codec.binary.Base64;
 
 public class Encryption {
-	
-	
-	
- public  static String Encode(String data) {
-	 String encryptedData = "";
-  try {
-   KeyStore keyStore = KeyStore.getInstance("JCEKS");
-   FileInputStream stream = new FileInputStream("C:/Users/Aditya/Desktop/Spring_Aditya/mykeystore.jks");
-   keyStore.load(stream, "password".toCharArray());
-   Key key = keyStore.getKey("mykey", "password".toCharArray());
-   
-   encryptedData = encryptWithAESKey(data, key.getEncoded());
-   
-   
-   return encryptedData;
 
-  } catch (Exception e) {
-   // TODO Auto-generated catch block
-   e.printStackTrace();
-  }
-  return "encoded";
- }
+	public static String Encode(String data) {
+		try {
+			KeyStore keyStore = KeyStore.getInstance("JCEKS");
+			FileInputStream stream = new FileInputStream("./keystore/mykeystore.jks");
+			keyStore.load(stream, "password".toCharArray());
+			Key key = keyStore.getKey("mykey", "password".toCharArray());
 
- 
- public  static String Decode(String encryptedData) {
-	  try {
-	   KeyStore keyStore = KeyStore.getInstance("JCEKS");
-	   FileInputStream stream = new FileInputStream("C:/Users/Aditya/Desktop/Spring_Aditya/mykeystore.jks");
-	   keyStore.load(stream, "password".toCharArray());
-	   Key key = keyStore.getKey("mykey", "password".toCharArray());
-	   
-	  
-	   return decryptWithAESKey(encryptedData, key.getEncoded());
+			return encryptWithAESKey(data, key.getEncoded());
 
-	  } catch (Exception e) {
-	   // TODO Auto-generated catch block
-	   e.printStackTrace();
-	  }
-	  return "encoded";
-	 }
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "encoding error";
+		}
 
- public static String encryptWithAESKey(String data, byte[] key) throws NoSuchAlgorithmException, NoSuchPaddingException,
-   InvalidKeyException, IllegalBlockSizeException, BadPaddingException, UnsupportedEncodingException {
-		  SecretKey secKey = new SecretKeySpec(key, "AES");
-		  //System.out.print("Key in encryption module: "+ key + "\n");
-		
-		  Cipher cipher = Cipher.getInstance("AES");
-		
-		  cipher.init(Cipher.ENCRYPT_MODE, secKey);
-		  byte[] newData = cipher.doFinal(data.getBytes());
-		
-		  return Base64.encodeBase64String(newData);
- }
+	}
 
- public static String decryptWithAESKey(String inputData, byte[] key) throws NoSuchAlgorithmException,
-		   NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-		  Cipher cipher = Cipher.getInstance("AES");
-		  SecretKey secKey = new SecretKeySpec(key, "AES");
-		  //System.out.print("Key in decryption module: "+ key + "\n");
-		
-		  cipher.init(Cipher.DECRYPT_MODE, secKey);
-		  byte[] newData = cipher.doFinal(Base64.decodeBase64(inputData.getBytes()));
-		  return new String(newData);
+	public static String Decode(String encryptedData) {
+		try {
+			KeyStore keyStore = KeyStore.getInstance("JCEKS");
+			FileInputStream stream = new FileInputStream("./keystore/mykeystore.jks");
+			keyStore.load(stream, "password".toCharArray());
+			Key key = keyStore.getKey("mykey", "password".toCharArray());
 
- }
+			return decryptWithAESKey(encryptedData, key.getEncoded());
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return "decoding error";
+		}
+
+	}
+
+	public static String encryptWithAESKey(String data, byte[] key)
+			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException,
+			BadPaddingException, UnsupportedEncodingException {
+		SecretKey secKey = new SecretKeySpec(key, "AES");
+
+		Cipher cipher = Cipher.getInstance("AES");
+
+		cipher.init(Cipher.ENCRYPT_MODE, secKey);
+		byte[] newData = cipher.doFinal(data.getBytes());
+
+		return Base64.encodeBase64String(newData);
+	}
+
+	public static String decryptWithAESKey(String inputData, byte[] key) throws NoSuchAlgorithmException,
+			NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+		Cipher cipher = Cipher.getInstance("AES");
+		SecretKey secKey = new SecretKeySpec(key, "AES");
+
+		cipher.init(Cipher.DECRYPT_MODE, secKey);
+		byte[] newData = cipher.doFinal(Base64.decodeBase64(inputData.getBytes()));
+		return new String(newData);
+
+	}
 
 }
